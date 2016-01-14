3460d0163c6b834ef790d10a35cceaba752e8f72
Rendxx <rendxx@gmail.com>
2016-01-14 11:03:04 -0600

    Touch position test complete

---
+++ TestMobile/TouchPos/sample.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    document.body.addEventListener('touchmove', function (event) {
+        event.preventDefault();
+    }, false);
+
+    var wrap = $('.wrap');
+    var monitor = $('.monitor');
+    var markerHtml = '<div class="marker"></div>';
+    var marker = {};
+    var position = {};
+
+    wrap[0].addEventListener('touchstart', function (e) {
+        var touch = event.changedTouches[0];
+        var s = "page (" + touch.pageX + ", " + touch.pageY + ")<br/>"
+              + "client (" + touch.clientX + ", " + touch.clientY + ")<br/>"
+              + "screen (" + touch.screenX + ", " + touch.screenY + ")";
+        console.log(s);
+        monitor.html(s);
+    }, false);
+});
